9596525a8643befc6083fecc958eb115481d1142
PatoSuar3z <pat.suarez@duocuc.cl>
2023-06-08 19:36:16 -0400

    Update : Index.js I need a IOS Developer account :c

---
+++ index.js
@@ -0,0 +1,9 @@
+const { PKPASS } = require("passkit-generator");
+var fs = require("fs");
+var path = require("path");
+var axios   = require('axios');
+
+
+exports.pass = function.https.onRequest((req, res) => {
+    PKPASS.from({})
+})
